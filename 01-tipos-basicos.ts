c3a298f6ad408054bfc16a80f0f05e91af04f8ff
imnicoo7 <61254982+imnicoo7@users.noreply.github.com>
2023-03-29 14:48:19 -0500

    tipos básicos variables

---
+++ 01-tipos-basicos.ts
@@ -0,0 +1,6 @@
+// variables typeScript identificarlas así
+let nombre: string = 'Strider';
+let hp: number | string = 'A95';
+let estaVivo: boolean = true;
+
+console.log(nombre, hp, estaVivo);
--- index.ts
+++ index.ts
@@ -1,6 +1,2 @@
-// variables typeScript identificarlas así
-let nombre: string = 'Strider';
-let hp: number | string = 'A95';
-let estaVivo: boolean = true;
 
-console.log(nombre, hp, estaVivo);
+
